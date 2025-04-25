03aebb2b47f3a6b04d2a2a22bed85c6933f33b6a
eightyfiver <arifthecoolmalayguy@gmail.com>
2025-04-26 03:52:18 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Sales Roleplay Game</h1>
+      <p>Welcome to the Sales Roleplay Game! 🎮</p>
+      <p>Objection and Scenario Cards will be here.</p>
+    </div>
+  );
+}
+
+export default App;
